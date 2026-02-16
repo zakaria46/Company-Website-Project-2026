588362161e04ba1e0bdc06f425a21a5026b74f80
Zakaria <43708727+zakaria46@users.noreply.github.com>
2026-02-16 17:58:51 +0200

    Finished the first two main sections

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     // $('#inner-caption').hide();
     $('#inner-caption').fadeIn(700).animate({
-    marginTop: "-20px" // This moves it 50 pixels UP from its current spot
+    marginTop: "-18px" // This moves it 50 pixels UP from its current spot
+    }, "slow");
+    $('#about-us').fadeIn(700).animate({
+    marginTop: "-25px" // This moves it 50 pixels UP from its current spot
     }, "slow");
     // $('#inner-caption').animate({
     // marginTop: "-=50px" // This moves it 50 pixels UP from its current spot
