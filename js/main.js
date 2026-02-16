e5e9e9b0800d7729182da1f90a8f28b5bb369847
Zakaria <43708727+zakaria46@users.noreply.github.com>
2026-02-16 15:33:19 +0200

    Added Hero backgroond and navbar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // $('#inner-caption').hide();
+    $('#inner-caption').fadeIn(700).animate({
+    marginTop: "-20px" // This moves it 50 pixels UP from its current spot
+    }, "slow");
+    // $('#inner-caption').animate({
+    // marginTop: "-=50px" // This moves it 50 pixels UP from its current spot
+    // }, "slow");
+});
