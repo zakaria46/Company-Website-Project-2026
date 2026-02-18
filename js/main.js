f5d833d18465bfd6c47fdae3f5461b85127d7f48
Zakaria <43708727+zakaria46@users.noreply.github.com>
2026-02-18 17:51:06 +0200

    Added a new section

--- js/main.js
+++ js/main.js
@@ -6,7 +6,25 @@ $(document).ready(function() {
     $('#about-us').fadeIn(700).animate({
     marginTop: "-25px" // This moves it 50 pixels UP from its current spot
     }, "slow");
-    // $('#inner-caption').animate({
-    // marginTop: "-=50px" // This moves it 50 pixels UP from its current spot
-    // }, "slow");
+    
+    // $('#sectors').fadeIn()
 });
+
+$(window).on('scroll', function() {
+    // jQuery's $(window).scrollTop() is the equivalent of window.pageYOffset
+    const scrollTop = $(window).scrollTop();
+    
+    // Logic for zoom calculation (100% check)
+    const zoomLevel = Math.round((window.outerWidth / window.innerWidth) * 100);
+
+    // if (scrollTop === 705.5999755859375 && zoomLevel === 100) {
+    //     $('#sectors').fadeIn(700).animate({
+    //          marginTop: "-25px" // This moves it 50 pixels UP from its current spot
+    //     }, "slow");
+    // }
+
+    // console.log(scrollTop);
+    // console.log(winHeight);
+});
+
+// Math.round((window.outerWidth / window.innerWidth) * 100)
